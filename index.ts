9794e17bb8a1e1b1736ba61593d0e971dd746b51
RAKIB HASAN <rakibhasan@RAKIBs-iMac.local>
2020-02-09 12:50:16 +0600

    render

--- index.ts
+++ index.ts
@@ -108,7 +108,7 @@ silence.save(function (err, silence) {
   if (err) return console.error(err);
   console.log(silence.time); // 'Silence'
 });
-res.send("100 ko")
+res.render("100 ko")
 })
 app.post("/extract", async(req, res) => {
  
